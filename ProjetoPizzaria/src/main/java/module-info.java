dff3f08be017c9487627b11a61db731bcbd1cb44
ViniDinz <110796583+ViniDinz@users.noreply.github.com>
2023-04-17 13:57:48 -0300

    Delete ProjetoPizzaria directory

--- ProjetoPizzaria/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main.java.ProjetoPizzaria {
-	requires javafx.base;
-    requires javafx.controls;
-    requires javafx.media;
-    requires javafx.web;
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.swing;
-}
